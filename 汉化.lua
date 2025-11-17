20f28e8df7940f6008f17b4c65b1d0b080848a76
1 <1@1.com>
2025-11-17 20:19:07 +0800

    1

--- 汉化.lua
+++ 汉化.lua
@@ -1,10 +1,8 @@
-local RF=gui.Reference
-
 local WeaponList={"通用","电击枪","手枪","沙鹰与R8","冲锋枪","步枪","散弹枪","鸟狙","连狙","大狙","机枪"}
 
-local function NM(RFName,SetName)  RFName:SetName(SetName)  end
+local RF=gui.Reference
 
----
+local function NM(RFName,SetName)  RFName:SetName(SetName)  end
 
 NM(RF("Legitbot"),"合法")
     NM(RF("合法", "Aimbot"),"自瞄")
@@ -13,5 +11,9 @@ NM(RF("Legitbot"),"合法")
             NM(RF("合法", "自瞄", "主要", "Aim Key"),"热键")
             NM(RF("合法", "自瞄", "主要", "Auto Pistol Interval"),"自动手枪间隔")
         NM(RF("合法", "自瞄", "Hitbox Selection"),"自瞄部位选择")
+            for i = 1, 11, 1 do
+                NM(RF("合法","自瞄","自瞄部位选择",WEN[i], "Hitbox Advance Multiplier"),"自瞄部位切换速度")
+                NM(RF("合法","自瞄","自瞄部位选择",WEN[i], "Nearest To Crosshair"),"准星附近优先")
+            end
     NM(RF("合法", "Triggerbot"),"扳机")
     NM(RF("合法", "Weapon"),"武器")
